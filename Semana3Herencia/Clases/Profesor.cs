d00b0d4079a807ec55c63c8acb5ff4398d737e56
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2022-05-05 20:01:51 -0600

    Primer Commit
    Subida de Archivos

---
+++ Semana3Herencia/Clases/Profesor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace Semana3Herencia.Clases
+{
+    public class Profesor
+    {
+        private string nombre;
+        private string identidad;
+        private string carrera;
+        private int horas_trabajadas;
+
+        public Profesor(string NombreParametro, string IdentidadParametro,
+                         string CarreraParametro)
+        {
+            nombre = NombreParametro;
+            identidad = IdentidadParametro;
+            carrera = CarreraParametro;
+            horas_trabajadas = 0;
+
+        }
+
+        /* Metodo Get es para retorna informacion de una Propiedad*/
+        public string GetNombre() {
+            return nombre;
+        }
+
+        /* Es para Actualizar Una Propiedad*/
+        public void SetNombre(string NombreNuevo) {
+            nombre = NombreNuevo;
+        }
+
+        public void ImpartirClases(int HorasTrabajadas) {
+
+            horas_trabajadas = horas_trabajadas + HorasTrabajadas;
+
+        }
+
+        public string Saludar() {
+
+            return "Hola mi nombre es " + nombre
+                + ",  mis horas trabajadas son: " + horas_trabajadas;
+
+        }
+
+
+
+    }
+}
---
+++ Semana3Herencia/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Semana3Herencia.Clases;
+
+namespace Semana3Herencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Profesor p1 = new Profesor("Fernando Lopez", "0801", "Diseño Web");
+
+            Console.WriteLine(p1.GetNombre());
+
+            p1.SetNombre("Fernando Raul Lopez Castro");
+
+            Console.WriteLine(p1.GetNombre());
+
+        }
+    }
+}
